7e3786535b8eb9b043eb0b688e15f76d9ab90a72
devinw50 <144460843+devinw50@users.noreply.github.com>
2023-12-14 09:31:42 -0500

    erte
    ert

--- vite-project/js/api.js
+++ vite-project/js/api.js
@@ -1,28 +1,47 @@
-const URL = 'https://valorant-api.com/v1/agents';
+const DOMSelectors = {
+    column: document.querySelector(".column"),
+    searchBtn: document.querySelector('.search-btn'),
+};
+
+const apiUrl = 'https://valorant-api.com/v1/agents';
+
 async function getData(url) {
     try {
         const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
         const data = await response.json();
-        return data; 
-    } catch (error) {
-        console.error("Error", error);
-    }
+        console.log(data.data); // Check the structure of the returned data
 
+        function insertCards(arr) {
+            arr.forEach((agent) => {
+                DOMSelectors.column.insertAdjacentHTML(
+                    "beforeend",
+                    `<div class="card">
+                        <h3 class="name">${agent.displayName}</h3>
+                        <img src="${agent.displayIcon}" class="img">
+                        <h4>Role: ${agent.role.displayName}</h4>
+                    </div>`
+                );
+            });
+        }
+
+        insertCards(data.data);
+
+        DOMSelectors.searchBtn.addEventListener('click', function () {
+            const agentName = prompt("Enter agent name:");
+            if (agentName) {
+                const filteredAgents = data.data.filter((agent) => agent.displayName.toLowerCase() === agentName.toLowerCase());
+                DOMSelectors.column.innerHTML = '';
+                insertCards(filteredAgents);
+            }
+        });
+
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        document.querySelector("h1").textContent = "error";
+    }
 }
 
-getData(URL)
-    .then(data => {
-        console.log(data);
-        data.Array.forEach((uuid)=> console.log(uuid))
-    });
-    
- function cards (arr){
-    arr.forEach(agents =>{
-        DOMSelectors.column.insertAdjacentHTML (
-            console.log('Agent Name: ${agent.displayName}'),
-            console.log('Role: ${agent.role.displayName}'),
-            console.log('Agent Name: ${agent.description}')
-        )
-        
-    })
- }
+getData(apiUrl);
